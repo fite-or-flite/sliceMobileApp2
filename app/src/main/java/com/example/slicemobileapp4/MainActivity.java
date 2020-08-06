6d56ede1c589bda133af062fd25807089bf4b79f
fite-or-flite <chrisfite00@gmail.com>
2020-08-06 11:47:50 -0500

    tightened up main activity onclicklisteners

--- app/src/main/java/com/example/slicemobileapp4/MainActivity.java
+++ app/src/main/java/com/example/slicemobileapp4/MainActivity.java
@@ -7,8 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,60 +24,45 @@ public class MainActivity extends AppCompatActivity {
         Button sidesButton = findViewById(R.id.sidesButton);
         Button slicesButton = findViewById(R.id.slicesButton);
 
-        pizzaButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), pizza.class);
-                startActivity(intent);
-            }
-        });
+        pizzaButton.setOnClickListener(this);
+        calzoneButton.setOnClickListener(this);
+        pastaButton.setOnClickListener(this);
+        saladsButton.setOnClickListener(this);
+        sweetsButton.setOnClickListener(this);
+        sidesButton.setOnClickListener(this);
+        slicesButton.setOnClickListener(this);
 
-        calzoneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), calzone.class);
-                startActivity(intent);
-            }
-        });
-
-        pastaButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), pasta.class);
-                startActivity(intent);
-            }
-        });
-
-        saladsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), salads.class);
-                startActivity(intent);
-            }
-        });
-        sweetsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), sweets.class);
-                startActivity(intent);
-            }
-        });
-
-        sidesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), sides.class);
-                startActivity(intent);
-            }
-        });
-
-        slicesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), slices.class);
-                startActivity(intent);
-            }
-        });
     }
-
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch(view.getId()) {
+            case R.id.pizzaButton:
+                intent = new Intent(view.getContext(), pizza.class);
+                break;
+            case R.id.calzoneButton:
+                intent = new Intent(view.getContext(), calzone.class);
+                break;
+            case R.id.pastaButton:
+                intent = new Intent(view.getContext(), pasta.class);
+                break;
+            case R.id.saladsButton:
+                intent = new Intent(view.getContext(), salads.class);
+                break;
+            case R.id.sidesButton:
+                intent = new Intent(view.getContext(), sides.class);
+                break;
+            case R.id.sweetsButton:
+                intent = new Intent(view.getContext(), sweets.class);
+                break;
+            case R.id.slicesButton:
+                intent = new Intent(view.getContext(), slices.class);
+                break;
+            default:
+                Toast.makeText(MainActivity.this, "Action not recognized. Try again.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        if (intent != null)
+            startActivity(intent);
+    }
 }
